63b903be5d3b762e09f7430cfdf8180ba54e7463
Gytis Andriejunas <andriejunas@gmail.com>
2023-12-08 12:07:33 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Shaders Galaxy model
+
+Small project practising to use vertex and fragment shaders in glsl.
+
+## Getting Started
+
+```
+npm install
+
+npm run dev
+```
+
+### Preview
+
+![Example GIF](src/Galaxy.gif)
---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,10 @@
+  varying vec3 vColor;
+  
+  void main()
+        {
+            float strength = distance(gl_PointCoord, vec2(0.5));
+            strength = 1.0 - strength;
+            strength = pow(strength, 4.0);
+        
+            gl_FragColor = vec4(vColor, strength);
+        }
